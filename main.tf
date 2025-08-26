366882c20cfa5ff750bd27a1b7e09bc90d4d74eb
Rui Lopes <rgl@ruilopes.com>
2025-08-26 21:19:00 +0000

    upgrade dependencies

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.12.2"
+  required_version = "1.13.0"
   required_providers {
     # see https://registry.terraform.io/providers/hashicorp/random
     # see https://github.com/hashicorp/terraform-provider-random
@@ -18,7 +18,7 @@ terraform {
     # see https://github.com/bpg/terraform-provider-proxmox
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.81.0"
+      version = "0.82.1"
     }
   }
 }
@@ -46,12 +46,12 @@ variable "proxmox_pve_node_address" {
   type = string
 }
 
-# see https://registry.terraform.io/providers/bpg/proxmox/0.81.0/docs/data-sources/virtual_environment_vms
+# see https://registry.terraform.io/providers/bpg/proxmox/0.82.1/docs/data-sources/virtual_environment_vms
 data "proxmox_virtual_environment_vms" "debian_templates" {
   tags = ["debian-13", "template"]
 }
 
-# see https://registry.terraform.io/providers/bpg/proxmox/0.81.0/docs/data-sources/virtual_environment_vm
+# see https://registry.terraform.io/providers/bpg/proxmox/0.82.1/docs/data-sources/virtual_environment_vm
 data "proxmox_virtual_environment_vm" "debian_template" {
   node_name = data.proxmox_virtual_environment_vms.debian_templates.vms[0].node_name
   vm_id     = data.proxmox_virtual_environment_vms.debian_templates.vms[0].vm_id
@@ -113,7 +113,7 @@ data "cloudinit_config" "example" {
   }
 }
 
-# see https://registry.terraform.io/providers/bpg/proxmox/0.81.0/docs/resources/virtual_environment_file
+# see https://registry.terraform.io/providers/bpg/proxmox/0.82.1/docs/resources/virtual_environment_file
 resource "proxmox_virtual_environment_file" "example_ci_user_data" {
   content_type = "snippets"
   datastore_id = "local"
@@ -124,7 +124,7 @@ resource "proxmox_virtual_environment_file" "example_ci_user_data" {
   }
 }
 
-# see https://registry.terraform.io/providers/bpg/proxmox/0.81.0/docs/resources/virtual_environment_vm
+# see https://registry.terraform.io/providers/bpg/proxmox/0.82.1/docs/resources/virtual_environment_vm
 resource "proxmox_virtual_environment_vm" "example" {
   name      = var.prefix
   node_name = "pve"
